111bbb89fd45481306d9333d906b241b236bcc10
Your NameIgor-Kay <kaygorodtsevie@gmail.com>
2024-04-17 22:49:20 +0700

    0.0.0

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "AirBnbProject",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
